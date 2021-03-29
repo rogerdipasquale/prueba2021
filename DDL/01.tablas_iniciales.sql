aa7f11f2846248da0382694f0a46699dcdfa8a4f
Rogelio Di Pasquale <rogelio.dipasquale@intive-fdv.com>
2021-03-29 19:29:42 -0300

    Commit inicial

---
+++ DDL/01.tablas_iniciales.sql
@@ -0,0 +1,13 @@
+create table a (
+	  id int not null,
+	  nombre1 varchar(50),
+	  nombre2 varchar(50)
+);
+
+create table b (
+	  id int not null,
+	  nombre1 varchar(50),
+	  nombre2 varchar(50)
+);
+
+
